745b974472c8c0e4dedb7c6b4b1612c02f83f08c
Tiger1218 <tiger1218@foxmail.com>
2024-01-21 03:06:08 +0800

    fix: typo in run_master.sh and add params.json

--- run_master.sh
+++ run_master.sh
@@ -7,4 +7,4 @@
 #
 
 
-torchrun --nnodes=2 --nproc_per_node=4 --node_rank=0 --master_addr=$MASTER_ADDR --master_port=$MASTER_ADDR benchmark.py --ckpt_dir llama-2-70b/ --tokenizer_path ./llama-2-70b/tokenizer.model --max_seq_len 512 --max_batch_size 6
+torchrun --nnodes=2 --nproc_per_node=4 --node_rank=0 --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT benchmark.py --ckpt_dir llama-2-70b/ --tokenizer_path ./llama-2-70b/tokenizer.model --max_seq_len 512 --max_batch_size 6
