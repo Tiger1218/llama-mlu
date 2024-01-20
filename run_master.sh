f5dedf6abc7718154a62cc2a347eea50c968c394
Tiger1218 <tiger1218@foxmail.com>
2024-01-21 02:55:10 +0800

    feat: add run_master.sh, run_slave.sh

---
+++ run_master.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# run_master.sh
+# Copyright (C) 2024 tiger1218 <tiger1218@Buwuhou-s-Archlinux>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+torchrun --nnodes=2 --nproc_per_node=4 --node_rank=0 --master_addr=$MASTER_ADDR --master_port=$MASTER_ADDR benchmark.py --ckpt_dir llama-2-70b/ --tokenizer_path ./llama-2-70b/tokenizer.model --max_seq_len 512 --max_batch_size 6
---
+++ run_slave.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# run_slave.sh
+# Copyright (C) 2024 tiger1218 <tiger1218@Buwuhou-s-Archlinux>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+torchrun --nnodes=2 --nproc_per_node=4 --node_rank=1 --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT benchmark.py --ckpt_dir llama-2-70b/ --tokenizer_path ./llama-2-70b/tokenizer.model --max_seq_len 512 --max_batch_size 6
